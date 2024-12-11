f619e7c68784a6c29e5ee5825d8dc3d9d1a10ce9
Olena-Berezenska <elena.yefimchuk@gmail.com>
2024-12-10 23:17:02 -0700

    Update task-2.js

--- js/task-2.js
+++ js/task-2.js
@@ -9,7 +9,7 @@
 // Якщо довжина перевищує maxLength, то функція обрізає рядок до maxLength символів, додає трикрапку "..." в кінці та повертає обрізану версію.
 
 function formatMessage(message, maxLength) {
-    return message.Length <= maxLength ? message : `${message.slice(0, maxLength)}...`
+    return message.length <= maxLength ? message : `${message.slice(0, maxLength)}...`
 }
 console.log(formatMessage("Curabitur ligula sapien", 16)); // "Curabitur ligula..."
 console.log(formatMessage("Curabitur ligula sapien", 23)); // "Curabitur ligula sapien"
